aaae759446fc28c50be2803bb122e986ad28915b
Istvan Csomortani <istvan.csomortani@analog.com>
2018-12-12 12:37:03 +0000

    quartus_pro: Global assignment ENABLE_ADVANCED_IO_TIMING is not supported

--- projects/scripts/adi_project_alt.tcl
+++ projects/scripts/adi_project_alt.tcl
@@ -152,7 +152,6 @@ proc adi_project_altera {project_name {parameter_list {}}} {
   # globals
 
   set_global_assignment -name SYNCHRONIZER_IDENTIFICATION AUTO
-  set_global_assignment -name ENABLE_ADVANCED_IO_TIMING ON
   set_global_assignment -name USE_TIMEQUEST_TIMING_ANALYZER ON
   set_global_assignment -name TIMEQUEST_DO_REPORT_TIMING ON
   set_global_assignment -name TIMEQUEST_DO_CCPP_REMOVAL ON
